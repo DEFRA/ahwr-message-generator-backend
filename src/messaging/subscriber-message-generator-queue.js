9f82f3256e8e38253d0c495996155af9ad459e7a
aaroncarroll82 <20041831+aaroncarroll82@users.noreply.github.com>
2025-12-30 12:32:09 +0000

    feat(AHWR-1452): Add metric counts for inbound messages received

--- src/messaging/subscriber-message-generator-queue.js
+++ src/messaging/subscriber-message-generator-queue.js
@@ -4,6 +4,7 @@ import { getLogger } from '../common/helpers/logging/logger.js'
 import { routeStatusUpdateMessage } from './router-status-change.js'
 import { processReminderEmailMessage } from '../processing/reminder-email-processor.js'
 import { processNewAgreementCreated } from '../processing/new-agreement-processor.js'
+import { metricsCounter } from '../common/helpers/metrics.js'
 
 let messageRequestSubscriber
 
@@ -35,12 +36,16 @@ export async function handleInboundMessage(message, attributes, types, logger, d
 
   switch (attributes.eventType) {
     case types.documentCreated:
+      await metricsCounter('event-received-document-created')
       return processNewAgreementCreated(message, logger, db)
     case types.statusUpdate:
+      await metricsCounter('event-received-claim-status-update')
       return routeStatusUpdateMessage(message, logger, db)
     case types.reminderRequest:
+      await metricsCounter('event-received-reminder-request')
       return processReminderEmailMessage(message, logger, db)
     default:
+      await metricsCounter('event-received-unknown-event-type')
       throw new Error(`Unsupported event received ${attributes.eventType}`)
   }
 }
