c0a576b71b3e11d9e0acc6e6fd178b3140eceb64
Ben Hope <bendef24@gmail.com>
2026-04-23 11:15:08 +0100

    feat(AHWR-1710): add support for poultry claim emails (#34)

--- src/processing/new-claim-created-processor.js
+++ src/processing/new-claim-created-processor.js
@@ -1,5 +1,11 @@
+import { getScheme, POULTRY_SCHEME } from 'ffc-ahwr-common-library'
 import { getLatestContactDetails } from '../api/application-api.js'
-import { AddressType, getHerdNameLabel, LIVESTOCK_TO_READABLE_SPECIES } from '../constants.js'
+import {
+  AddressType,
+  getHerdNameLabel,
+  LIVESTOCK_TO_READABLE_SPECIES,
+  POULTRY_TO_READABLE_SPECIES
+} from '../constants.js'
 import { config } from '../config.js'
 import { sendSFDCommsRequest } from '../messaging/send-sfd-comms-request.js'
 import {
@@ -10,6 +16,26 @@ import {
 const MESSAGE_TYPE = 'claimCreated'
 
 export const processNewClaimCreated = async (message, logger, db) => {
+  const { agreementReference, claimReference } = message
+
+  const messageAlreadyGenerated = await getByClaimRefAndMessageType(
+    db,
+    claimReference,
+    MESSAGE_TYPE
+  )
+  if (messageAlreadyGenerated) {
+    logger.info('Message has already been processed for claim being created')
+  } else {
+    const contactDetails = await getLatestContactDetails(agreementReference, logger)
+    if (getScheme(agreementReference) === POULTRY_SCHEME) {
+      await processPoultryNewClaimMessage(message, contactDetails, db, logger)
+    } else {
+      await processLivestockNewClaimMessage(message, contactDetails, db, logger)
+    }
+  }
+}
+
+const processLivestockNewClaimMessage = async (message, contactDetails, db, logger) => {
   const { newReviewClaimTemplateId, newFollowUpClaimTemplateId } = config.get('notify.templates')
   const noReplyEmailReplyToId = config.get('notify.replyToIdNoReply')
   const carbonCopyEmailAddress = config.get('notify.carbonCopyEmailAddress')
@@ -25,78 +51,67 @@ export const processNewClaimCreated = async (message, logger, db) => {
     herdName,
     claimAmount
   } = message
+  const { name: orgName, orgEmail, email } = contactDetails
 
-  const messageAlreadyGenerated = await getByClaimRefAndMessageType(
-    db,
+  const requestParams = {
+    agreementReference,
     claimReference,
-    MESSAGE_TYPE
-  )
+    crn,
+    sbi,
+    claimType,
+    typeOfLivestock,
+    reviewTestResults,
+    logger,
+    herdName,
+    claimAmount,
+    emailReplyToId: noReplyEmailReplyToId,
+    notifyTemplateId:
+      claimType === 'FOLLOW_UP' ? newFollowUpClaimTemplateId : newReviewClaimTemplateId
+  }
 
-  if (messageAlreadyGenerated) {
-    logger.info('Message has already been processed for claim being created')
-  } else {
-    const contactDetails = await getLatestContactDetails(agreementReference, logger)
-    const { name: orgName, orgEmail, email } = contactDetails
-    const requestParams = {
-      agreementReference,
-      claimReference,
-      crn,
-      sbi,
-      claimType,
-      typeOfLivestock,
-      reviewTestResults,
-      logger,
-      herdName,
-      claimAmount,
-      emailReplyToId: noReplyEmailReplyToId,
-      notifyTemplateId:
-        claimType === 'FOLLOW_UP' ? newFollowUpClaimTemplateId : newReviewClaimTemplateId
-    }
-
-    if (carbonCopyEmailAddress) {
-      await sendClaimConfirmationEmail({
-        ...requestParams,
-        emailAddress: carbonCopyEmailAddress,
-        addressType: AddressType.CC
-      })
-    }
-
-    if (orgEmail) {
-      await sendClaimConfirmationEmail({
-        ...requestParams,
-        emailAddress: orgEmail,
-        addressType: AddressType.ORG_EMAIL
-      })
-    }
-
-    if (email && email !== orgEmail) {
-      await sendClaimConfirmationEmail({
-        ...requestParams,
-        emailAddress: email,
-        addressType: AddressType.EMAIL
-      })
-    }
-
-    await createMessageRequestEntry(db, {
-      agreementReference,
-      claimReference,
-      messageType: MESSAGE_TYPE,
-      data: {
-        crn,
-        sbi,
-        orgName,
-        claimType,
-        typeOfLivestock,
-        email,
-        orgEmail,
-        herdName,
-        claimAmount
-      }
+  if (carbonCopyEmailAddress) {
+    await sendLivestockClaimConfirmationEmail({
+      ...requestParams,
+      emailAddress: carbonCopyEmailAddress,
+      addressType: AddressType.CC
     })
   }
+
+  if (orgEmail) {
+    await sendLivestockClaimConfirmationEmail({
+      ...requestParams,
+      emailAddress: orgEmail,
+      addressType: AddressType.ORG_EMAIL
+    })
+  }
+
+  if (email && email !== orgEmail) {
+    await sendLivestockClaimConfirmationEmail({
+      ...requestParams,
+      emailAddress: email,
+      addressType: AddressType.EMAIL
+    })
+  }
+
+  await createMessageRequestEntry(db, {
+    agreementReference,
+    claimReference,
+    messageType: MESSAGE_TYPE,
+    data: {
+      crn,
+      sbi,
+      orgName,
+      claimType,
+      typeOfLivestock,
+      email,
+      orgEmail,
+      herdName,
+      claimAmount
+    }
+  })
 }
 
-const sendClaimConfirmationEmail = async (params) => {
+const sendLivestockClaimConfirmationEmail = async (params) => {
   const {
     crn,
     sbi,
@@ -145,6 +160,149 @@ const sendClaimConfirmationEmail = async (params) => {
         outcome: 'true',
         kind: addressType,
         category: `${typeOfLivestock} - templateId:${notifyTemplateId}`
+      }
+    })
+
+    logger.info(`Sent ${addressType} new claim email`)
+  } catch (error) {
+    logger.error(
+      {
+        error,
+        event: {
+          type: 'exception',
+          category: 'failed-processing'
+        }
+      },
+      `Error sending ${addressType} new claim email.`
+    )
+    throw error
+  }
+}
+
+const processPoultryNewClaimMessage = async (message, contactDetails, db, logger) => {
+  const { poultryNewReviewClaimTemplateId } = config.get('notify.templates')
+  const noReplyEmailReplyToId = config.get('notify.replyToIdNoReply')
+  const carbonCopyEmailAddress = config.get('notify.carbonCopyEmailAddress')
+
+  const {
+    agreementReference,
+    claimReference,
+    sbi,
+    crn,
+    claimType,
+    typesOfPoultry,
+    herdName,
+    claimAmount
+  } = message
+  const { name: orgName, orgEmail, email } = contactDetails
+
+  const requestParams = {
+    agreementReference,
+    claimReference,
+    crn,
+    sbi,
+    claimType,
+    typesOfPoultry,
+    logger,
+    herdName,
+    claimAmount,
+    emailReplyToId: noReplyEmailReplyToId,
+    notifyTemplateId: poultryNewReviewClaimTemplateId
+  }
+
+  if (carbonCopyEmailAddress) {
+    await sendPoultryClaimConfirmationEmail({
+      ...requestParams,
+      emailAddress: carbonCopyEmailAddress,
+      addressType: AddressType.CC
+    })
+  }
+
+  if (orgEmail) {
+    await sendPoultryClaimConfirmationEmail({
+      ...requestParams,
+      emailAddress: orgEmail,
+      addressType: AddressType.ORG_EMAIL
+    })
+  }
+
+  if (email && email !== orgEmail) {
+    await sendPoultryClaimConfirmationEmail({
+      ...requestParams,
+      emailAddress: email,
+      addressType: AddressType.EMAIL
+    })
+  }
+
+  await createMessageRequestEntry(db, {
+    agreementReference,
+    claimReference,
+    messageType: MESSAGE_TYPE,
+    data: {
+      crn,
+      sbi,
+      orgName,
+      claimType,
+      typesOfPoultry,
+      email,
+      orgEmail,
+      herdName,
+      claimAmount
+    }
+  })
+}
+
+const sendPoultryClaimConfirmationEmail = async (params) => {
+  const {
+    crn,
+    sbi,
+    emailAddress,
+    addressType,
+    agreementReference,
+    claimReference,
+    typesOfPoultry,
+    claimAmount,
+    herdName,
+    notifyTemplateId,
+    emailReplyToId,
+    logger
+  } = params
+
+  logger.info(`Sending ${addressType} poultry new claim email`)
+
+  const species = typesOfPoultry.map((poultry) => POULTRY_TO_READABLE_SPECIES[poultry]).join(', ')
+
+  try {
+    const customParams = {
+      reference: claimReference,
+      applicationReference: agreementReference,
+      amount: claimAmount,
+      species,
+      crn,
+      sbi,
+      herdNameLabel: 'Site',
+      herdName
+    }
+
+    await sendSFDCommsRequest({
+      crn,
+      sbi,
+      agreementReference,
+      claimReference,
+      notifyTemplateId,
+      emailReplyToId,
+      emailAddress,
+      customParams,
+      logger
+    })
+
+    logger.info({
+      event: {
+        type: `claim-email-requested`,
+        reference: claimReference,
+        outcome: 'true',
+        kind: addressType,
+        category: `poultry - templateId:${notifyTemplateId}`
       }
     })
 
--- src/processing/new-claim-created-processor.test.js
+++ src/processing/new-claim-created-processor.test.js
@@ -26,6 +26,7 @@ describe('process new claim email message', () => {
     config.set('notify.replyToIdNoReply', 'no-reply@example.com')
     config.set('notify.templates.newReviewClaimTemplateId', 'review-complete-template-id')
     config.set('notify.templates.newFollowUpClaimTemplateId', 'followup-complete-template-id')
+    config.set('notify.templates.poultryNewReviewClaimTemplateId', 'poultry-review-template-id')
   })
 
   afterEach(() => {
@@ -46,46 +47,6 @@ describe('process new claim email message', () => {
     herdName: 'Commercial herd'
   }
 
-  const checkNewClaimEmailSendAndEventRaised = (
-    emailAddress,
-    notifyTemplateId,
-    addressType,
-    species = 'Beef cattle',
-    herdNameLabel = 'Herd name',
-    claimReference = 'REBC-O9UD-22F6'
-  ) => {
-    expect(sendSFDCommsRequest).toHaveBeenCalledWith({
-      emailAddress,
-      emailReplyToId: 'no-reply@example.com',
-      agreementReference: 'IAHW-0AD3-3322',
-      claimReference,
-      crn: '1100014934',
-      sbi: '106705779',
-      notifyTemplateId,
-      customParams: {
-        sbi: '106705779',
-        crn: '1100014934',
-        applicationReference: 'IAHW-0AD3-3322',
-        reference: claimReference,
-        amount: 500,
-        herdName: 'Commercial herd',
-        herdNameLabel,
-        species
-      },
-      logger: mockedLogger
-    })
-
-    expect(mockedLogger.info).toHaveBeenCalledWith({
-      event: {
-        type: 'claim-email-requested',
-        outcome: 'true',
-        reference: claimReference,
-        kind: addressType,
-        category: expect.stringMatching(`- templateId:${notifyTemplateId}`)
-      }
-    })
-  }
-
   test('should send a new claim email when it is the first time the claim has passed through', async () => {
     getByClaimRefAndMessageType.mockResolvedValueOnce(null)
     getLatestContactDetails.mockResolvedValueOnce({
@@ -104,13 +65,17 @@ describe('process new claim email message', () => {
     )
     expect(getLatestContactDetails).toHaveBeenCalledWith('IAHW-0AD3-3322', mockedLogger)
     expect(sendSFDCommsRequest).toHaveBeenCalledTimes(3)
-    checkNewClaimEmailSendAndEventRaised('cc@example.com', 'review-complete-template-id', 'CC')
-    checkNewClaimEmailSendAndEventRaised(
+    checkLivestockNewClaimEmailSendAndEventRaised(
+      'cc@example.com',
+      'review-complete-template-id',
+      'CC'
+    )
+    checkLivestockNewClaimEmailSendAndEventRaised(
       'willowfarm@gmail.com',
       'review-complete-template-id',
       'orgEmail'
     )
-    checkNewClaimEmailSendAndEventRaised(
+    checkLivestockNewClaimEmailSendAndEventRaised(
       'john.doe@gmail.com',
       'review-complete-template-id',
       'email'
@@ -151,7 +116,7 @@ describe('process new claim email message', () => {
     )
     expect(getLatestContactDetails).toHaveBeenCalledWith('IAHW-0AD3-3322', mockedLogger)
     expect(sendSFDCommsRequest).toHaveBeenCalledTimes(1)
-    checkNewClaimEmailSendAndEventRaised(
+    checkLivestockNewClaimEmailSendAndEventRaised(
       'john.doe@gmail.com',
       'review-complete-template-id',
       'email'
@@ -195,7 +160,7 @@ describe('process new claim email message', () => {
     )
     expect(getLatestContactDetails).toHaveBeenCalledWith('IAHW-0AD3-3322', mockedLogger)
     expect(sendSFDCommsRequest).toHaveBeenCalledTimes(2)
-    checkNewClaimEmailSendAndEventRaised(
+    checkLivestockNewClaimEmailSendAndEventRaised(
       'cc@example.com',
       'followup-complete-template-id',
       'CC',
@@ -203,7 +168,7 @@ describe('process new claim email message', () => {
       'Flock name',
       'FUSH-O9UD-22F6'
     )
-    checkNewClaimEmailSendAndEventRaised(
+    checkLivestockNewClaimEmailSendAndEventRaised(
       'willowfarm@gmail.com',
       'followup-complete-template-id',
       'orgEmail',
@@ -283,4 +248,185 @@ describe('process new claim email message', () => {
       `Error sending CC new claim email.`
     )
   })
+
+  test('should send a new claim email for poultry', async () => {
+    const poultryEventBody = {
+      crn: '1100000000',
+      sbi: '100000000',
+      agreementReference: 'POUL-CH1C-K3NS',
+      claimReference: 'PORE-CH1C-K3NS',
+      claimStatus: 'IN_CHECK',
+      claimType: 'REVIEW',
+      typesOfPoultry: ['laying-hens', 'breeders', 'ducks'],
+      claimAmount: 430,
+      herdName: 'Chicken Site'
+    }
+    getByClaimRefAndMessageType.mockResolvedValueOnce(null)
+    getLatestContactDetails.mockResolvedValueOnce({
+      name: 'Poultry Farm',
+      orgEmail: 'poultryfarm@example.com',
+      farmerName: 'Chic Ken',
+      email: 'chic.ken@example.com'
+    })
+
+    await processNewClaimCreated(poultryEventBody, mockedLogger, mockDb)
+
+    expect(getByClaimRefAndMessageType).toHaveBeenCalledWith(
+      mockDb,
+      'PORE-CH1C-K3NS',
+      'claimCreated'
+    )
+    expect(getLatestContactDetails).toHaveBeenCalledWith('POUL-CH1C-K3NS', mockedLogger)
+    expect(sendSFDCommsRequest).toHaveBeenCalledTimes(3)
+    checkPoultryNewClaimEmailSendAndEventRaised(
+      'cc@example.com',
+      'poultry-review-template-id',
+      'CC'
+    )
+    checkPoultryNewClaimEmailSendAndEventRaised(
+      'poultryfarm@example.com',
+      'poultry-review-template-id',
+      'orgEmail'
+    )
+    checkPoultryNewClaimEmailSendAndEventRaised(
+      'chic.ken@example.com',
+      'poultry-review-template-id',
+      'email'
+    )
+    expect(createMessageRequestEntry).toHaveBeenCalledWith(mockDb, {
+      agreementReference: 'POUL-CH1C-K3NS',
+      claimReference: 'PORE-CH1C-K3NS',
+      messageType: 'claimCreated',
+      data: {
+        orgName: 'Poultry Farm',
+        orgEmail: 'poultryfarm@example.com',
+        email: 'chic.ken@example.com',
+        crn: '1100000000',
+        sbi: '100000000',
+        claimType: 'REVIEW',
+        typesOfPoultry: ['laying-hens', 'breeders', 'ducks'],
+        claimAmount: 430,
+        herdName: 'Chicken Site'
+      }
+    })
+  })
+
+  test('should track an exception when email fails to send for poultry', async () => {
+    const poultryEventBody = {
+      crn: '1100000000',
+      sbi: '100000000',
+      agreementReference: 'POUL-CH1C-K3NS',
+      claimReference: 'PORE-CH1C-K3NS',
+      claimStatus: 'IN_CHECK',
+      claimType: 'REVIEW',
+      typesOfPoultry: ['laying-hens', 'breeders', 'ducks'],
+      claimAmount: 430,
+      herdName: 'Chicken Site'
+    }
+    const error = new Error('Email send failed')
+    sendSFDCommsRequest.mockImplementationOnce(() => {
+      throw error
+    })
+
+    getByClaimRefAndMessageType.mockResolvedValueOnce(null)
+    getLatestContactDetails.mockResolvedValueOnce({
+      name: 'Poultry Farm',
+      orgEmail: 'poultryfarm@example.com'
+    })
+
+    await expect(processNewClaimCreated(poultryEventBody, mockedLogger, mockDb)).rejects.toThrow(
+      'Email send failed'
+    )
+
+    expect(mockedLogger.error).toHaveBeenCalledWith(
+      {
+        error,
+        event: {
+          category: 'failed-processing',
+          type: 'exception'
+        }
+      },
+      `Error sending CC new claim email.`
+    )
+  })
+
+  const checkLivestockNewClaimEmailSendAndEventRaised = (
+    emailAddress,
+    notifyTemplateId,
+    addressType,
+    species = 'Beef cattle',
+    herdNameLabel = 'Herd name',
+    claimReference = 'REBC-O9UD-22F6'
+  ) => {
+    expect(sendSFDCommsRequest).toHaveBeenCalledWith({
+      emailAddress,
+      emailReplyToId: 'no-reply@example.com',
+      agreementReference: 'IAHW-0AD3-3322',
+      claimReference,
+      crn: '1100014934',
+      sbi: '106705779',
+      notifyTemplateId,
+      customParams: {
+        sbi: '106705779',
+        crn: '1100014934',
+        applicationReference: 'IAHW-0AD3-3322',
+        reference: claimReference,
+        amount: 500,
+        herdName: 'Commercial herd',
+        herdNameLabel,
+        species
+      },
+      logger: mockedLogger
+    })
+
+    expect(mockedLogger.info).toHaveBeenCalledWith({
+      event: {
+        type: 'claim-email-requested',
+        outcome: 'true',
+        reference: claimReference,
+        kind: addressType,
+        category: expect.stringMatching(`- templateId:${notifyTemplateId}`)
+      }
+    })
+  }
+
+  const checkPoultryNewClaimEmailSendAndEventRaised = (
+    emailAddress,
+    notifyTemplateId,
+    addressType,
+    species = 'Laying hens, Breeders, Ducks',
+    herdNameLabel = 'Site',
+    claimReference = 'PORE-CH1C-K3NS'
+  ) => {
+    expect(sendSFDCommsRequest).toHaveBeenCalledWith({
+      emailAddress,
+      emailReplyToId: 'no-reply@example.com',
+      agreementReference: 'POUL-CH1C-K3NS',
+      claimReference,
+      crn: '1100000000',
+      sbi: '100000000',
+      notifyTemplateId,
+      customParams: {
+        sbi: '100000000',
+        crn: '1100000000',
+        applicationReference: 'POUL-CH1C-K3NS',
+        reference: claimReference,
+        amount: 430,
+        herdName: 'Chicken Site',
+        herdNameLabel,
+        species
+      },
+      logger: mockedLogger
+    })
+
+    expect(mockedLogger.info).toHaveBeenCalledWith({
+      event: {
+        type: 'claim-email-requested',
+        outcome: 'true',
+        reference: claimReference,
+        kind: addressType,
+        category: expect.stringMatching(`- templateId:${notifyTemplateId}`)
+      }
+    })
+  }
 })
